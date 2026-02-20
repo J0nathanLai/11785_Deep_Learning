9f5fe5cc4fbb19e7774da5c9b3b60c809cdeaf0f
J0nathanLai <jklai@andrew.cmu.edu>
2026-02-20 15:57:48 -0500

    setup testing for resampling

---
+++ HW2/hw2p1_handout/mytorch/nn/__init__.py
@@ -0,0 +1,4 @@
+from .activation import Identity, Sigmoid, Tanh, ReLU, GELU, Swish, Softmax
+from .linear import Linear
+from .loss import MSELoss, CrossEntropyLoss
+from resampling import Upsample1d, Downsample1d
